c2252af240ef853752a069e66ae123bd1ef48b25
ruskinbind <113052074+ruskinbind@users.noreply.github.com>
2026-05-12 20:24:47 +0530

    Add files via upload

---
+++ Prismax/core/header.js
@@ -0,0 +1,14 @@
+const headers = {
+  Accept: "*/*",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+  "Content-Type": "application/json",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+  origin: "https://app.prismax.ai",
+  referer: "https://app.prismax.ai/",
+  "sec-ch-ua": '"Microsoft Edge";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+  "sec-fetch-site": "cross-site",
+  "sec-fetch-mode": "cors",
+};
+
+module.exports = { headers };
